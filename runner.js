5428f071f54f7dca33000e547adcf46a87f52bce
Jay <dev@jaywick.xyz>
2016-10-08 19:06:07 +1100

    Added player to canvas.

--- runner.js
+++ runner.js
@@ -1,1 +1,21 @@
-// watch this space
+var context, canvas;
+
+main = function() {
+    canvas = document.createElement("canvas");
+    canvas.width = 700;
+    canvas.height = 300;
+    canvas.style.backgroundColor = "whitesmoke";
+    canvas.style.border = "lightgray solid 1px"
+    document.body.style.textAlign = "center";
+    document.body.appendChild(canvas);
+    context = canvas.getContext("2d");
+
+    addCharacter(35, 50);
+}
+
+addCharacter = function(w, h) {
+    context.fillRect((canvas.width / 10), canvas.height - h, w, h);
+}
+
+
+main();
